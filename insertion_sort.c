5fe5bcd9d84693853891dd930371f1d4f939190e
NorthCreek <202406052@post.au.dk>
2024-10-27 21:15:02 +0100

    comments

--- insertion_sort.c
+++ insertion_sort.c
@@ -2,28 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-node* sorted_insert(node* sorted, node* new_node) {
-    if (sorted == NULL || sorted->data >= new_node->data) {
+node* sorted_insert(node* sorted, node* new_node) 
+{
+    // hvis sorted er NULL eller sorted som peger på data ikke er ligmed eller større end new_node som peger på data
+    // så er new_node der peger på next ligmed sorted
+    if (sorted == NULL || sorted->data >= new_node->data) 
+    {
         new_node->next = sorted;
         return new_node;
     }
-
+    // ellers så laves en ny node current som sættes til at være ligmed sorted herefter oprettes der en whileløkke 
+    // hvori så længe current som peger på next ikke er NULL og current der peger på data der peger på next
+    // er mindre end new_node der peger på data så skal current ved ligmed current der peger på next
     node* current = sorted;
-    while (current->next != NULL && current->next->data < new_node->data) {
+    while (current->next != NULL && current->next->data < new_node->data) 
+    {
         current = current->next;
     }
-
+    // new_node der peger på next skal være ligmed current der peger på next og current der peger på next skal være ligmed new_node
     new_node->next = current->next;
     current->next = new_node;
 
     return sorted;
 }
 
-node* isort(node* list) {
-    if (list == NULL || list->next == NULL) {
+node* isort(node* list) 
+{
+    // hvis vores list er tom returneres list da vi ikke skal sortere noget så
+    if (list == NULL || list->next == NULL) 
+    {
         return list; 
     }
-
+    // ellers så laves to nodes sorted=NULL og current=list som køres igennem en whileløkke
+    // hvori så længe current ikke er NULL så skal next være ligmed current der peger på next 
+    // og sorted skal være ligmed sorted_insert som er den void funktionen fra tidligere 
+    // herefter skal current være ligmed next
     node* sorted = NULL;  
     node* current = list; 
 
--- queue.c
+++ queue.c
@@ -22,15 +22,19 @@ bool full(const queue *q) {
 }
 
 void enqueue(queue *q, int x) {
+    // allocerer plads i hukommelsen med malloc til new_node
     node* new_node = (node*)malloc(sizeof(node));
    new_node->data = x;
    new_node->next = NULL;
-
+    // hvis køens sidste element er NULL altså hvis køen er tom 
+    // så skal både køens første element og sidste element være new_node så vi har et enkelt elemnt i vores kø
    if (q->rear == NULL)
     {
      q->front = new_node;
      q->rear = new_node;
     }
+    // hvis så køen IKKE er tom så skal køens sidste element være det næst tilføjede element
+    // altså et nyt element new_node og køens størrelse vokser
     else
     {
     q->rear->next = new_node;
@@ -40,18 +44,21 @@ void enqueue(queue *q, int x) {
 }
 
 int dequeue(queue *q) {
+    // hvis køen er tom stopper programmet da vi ikke kan tage et element fra en tom kø
     if (empty(q))
     {
         return -1;
     }
-        node* temp = q->front;
-        int data = temp->data;
-        q->front = q->front->next;
-
-        if (q->front == NULL)
-        {
+    // hvis køen så ikke er tom fjernes et element fra køen
+    node* temp = q->front;
+    int data = temp->data;
+    q->front = q->front->next;
+    // hvis køen så er tom nu efter vi har fjernet et element så frigøres der plads i hukommelsen
+    // ved at fjerne vores midlertidige element temp
+    if (q->front == NULL)
+    {
         q->rear = NULL;
-        }
+    }
 
     free(temp);
     q->size--;
@@ -60,17 +67,20 @@ int dequeue(queue *q) {
 
 // Opgave 4
 void push(int element, node **head) {
+    // laver plads i hukommelsen til en ny node som er en pointer til toppen af vores stack
     node *newNode = (node*)malloc(sizeof(node));
     newNode->next = *head;
     *head = newNode;
 }
 
 int pop(node **head) {
+    // vi forsøger at poppe et element fra vores stack og hvis toppen af stacken så er tom printes det ud
     if (*head == NULL)
     {
         printf("Stack er tom\n");
         return -1;
     }
+    // hvis stackens øverste element ikker NULL så poppes det øverste element
     int data = (*head)->data;
     node *temp = *head;
     *head = (*head)->next;
@@ -79,49 +89,33 @@ int pop(node **head) {
 }
 
 void enqueueStack(queue *q, int x) {
+    // vi skubber et element oven på stacken ved brug af vores kø funktion
     push(x, &(q->front));
 }
 
 int dequeueStack(queue *q) {
+    // vi laver en midlertidig stack med et midlertidigt top element som skal være tomt
     stack temp;
     temp.head = NULL;
-    
+    // hvis vores kø er tom så kan vi ikke skubbe elementer fra vores kø oven på vores stack 
+    // derfor printes der ud af stacken er tom
     if (q->front == NULL)
     {
         printf("Stack er tom\n");
         return -1;
     }
-    
+    // hvis der er mindst et element i vores kø så skubbes det oven på vores stack
     while (q->front != NULL)
     {
         int element = pop(&(q->front));
         push(element, &(temp.head));
     }
     int result = pop(&(temp.head));
-
+    // mens vores midlertidige stack så ikke er tom så vil vi poppe de øverste element fra stacken
+    //  ved at skubbe det ud med vores kø funktion
     while (temp.head != NULL)
     {
         push(pop(&(temp.head)), &(q->front));
     }
     return result;
-
-    int main(); {
-    queue q; 
-    q.front = NULL; 
-
-    enqueueStack(&q, 1);
-    enqueueStack(&q, 2);
-    enqueueStack(&q, 3);
-
-    printf("Dequeued: %d\n", dequeueStack(&q)); 
-    printf("Dequeued: %d\n", dequeueStack(&q)); 
-
-    enqueueStack(&q, 4);
-
-    printf("Dequeued: %d\n", dequeueStack(&q)); 
-    printf("Dequeued: %d\n", dequeueStack(&q)); 
-    printf("Dequeued: %d\n", dequeueStack(&q)); 
-
-    return 0;
-    }
 }
